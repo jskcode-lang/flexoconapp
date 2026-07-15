959cdd830f4ae9d6f2b048be8c05f7edd607a5f3
jskcode-lang <jsknowledgeofficial@gmail.com>
2026-07-15 13:58:54 +0530

    commit for the changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/flexoconapp/",
   server: {
     open: true,
     port: 3001,
